df9f8ea67fe398b948af4ca55fdd05c80debcec4
= <=>
2024-08-06 23:58:36 +0300

    added regex dependency and read for the invoice number

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use lopdf::Document;
 use std::fs::File;
 use std::io::{Read,Write};
 use rfd::FileDialog;
 use pdf_extract::extract_text;
 use tempfile::NamedTempFile;
+use regex::Regex;
 
 
 slint::include_modules!();
@@ -25,7 +25,30 @@ fn main() -> Result<(), slint::PlatformError> {
 
             // Extract text using pdf-extract
             match extract_text(temp_file.path()) {
-                Ok(text) => println!("{}", text),
+                Ok(text) => {
+                    println!("Extracted Text:\n{}", text);
+                    // Define regex patterns for dates and prices
+                    let date_re = Regex::new(r"\b\d{4}-\d{2}-\d{2}\b").expect("Invalid regex for dates");
+                    let price_re = Regex::new(r"\b\d+\.\d{2}\b").expect("Invalid regex for prices");
+                    // Define regex pattern for the invoice number
+                    let invoice_re = Regex::new(r"Invoice number/Αριθμός τιμολογίου: (\d+\[\d+\]|\d+)").expect("Invalid regex for invoice number");
+
+                    // Find and print the invoice number
+                    if let Some(captures) = invoice_re.captures(&text) {
+                        if let Some(invoice_number) = captures.get(1) {
+                            println!("Found invoice number: {}", invoice_number.as_str());
+                        }
+                    }
+                    // Find and print all dates
+                    for date in date_re.find_iter(&text) {
+                        println!("Found date: {}", date.as_str());
+                    }
+
+                    // Find and print all prices
+                    for price in price_re.find_iter(&text) {
+                        println!("Found price: {}", price.as_str());
+                    }
+                },
                 Err(e) => eprintln!("Failed to extract text: {}", e),
             }
             // ** TODO **
